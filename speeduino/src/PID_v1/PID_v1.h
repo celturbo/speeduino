a378e584354416f580c8acf769087127ca871a94
celso acyr <celso.acyr@outlook.com>
2020-05-12 22:40:05 -0300

    Closed loop idle control improvements
    Closed loop idle control improvements

--- speeduino/src/PID_v1/PID_v1.h
+++ speeduino/src/PID_v1/PID_v1.h
@@ -94,6 +94,9 @@ class integerPID
         byte, byte, byte, byte);     //   Setpoint.  Initial tuning parameters are also set here
 
     void SetMode(int Mode);               // * sets PID to either Manual (0) or Auto (non-0)
+    void setFroCrank( unsigned long); 
+    void SetPos();
+    void CapterPos();//Capture the current position in PID.
 
     bool Compute(bool pOnE);                       // * performs the PID calculation.  it should be
                                           //   called every time loop() cycles. ON/OFF and
@@ -142,7 +145,8 @@ class integerPID
                                   //   what these values are.  with pointers we'll just know.
 
 	unsigned long lastTime;
-	long outputSum, lastInput, lastMinusOneInput;
+	long outputSum, lastInput, lastMinusOneInput,LastoutputSum;
+  bool control;
   int16_t lastError;
 
 	uint16_t SampleTime;
